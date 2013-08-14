af1e944ae7e1d1e01d1483bd9b4d58d9db354f37
Conroy <conroy@quirky.com>
2013-08-14 14:21:13 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GringottsClient::Application.config.secret_token = 'eea0d754c14c2b7f9e7a5a1f075b13c2bce5d4b40fa1aadb1ab49395e47613815a478f572ba06df3070d2ddb11bc48b6ab3d6c5ea99cb1376e5c3c1b09c30080'
