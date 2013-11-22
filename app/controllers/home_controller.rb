bfc9fd1d38c4a9d676bbc185cf264b7d0778b067
Conroy Whitney <conroy.whitney@gmail.com>
2013-11-22 07:29:22 +0000

    persistence -- be able to change homepage CTA based on user state

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
+    steps = {
+      signup: {
+        greeting: "O hai!",
+        cta: "Step #1: Create an Account",
+        target: "/users/sign_up"
+      }
+    }
+    
+    # TODO: change this based on user verification / persistence state
+    @current_step = steps[:signup]
   end
 end
