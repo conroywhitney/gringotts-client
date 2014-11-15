9d3cb4fb7aa234b234f9effb61fe9bda85735407
Conroy Whitney <conroy.whitney@gmail.com>
2014-11-15 16:43:36 -0500

    config -- secret token default xxxx in development/test

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,12 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-GringottsClient::Application.config.secret_key_base = ENV["SECRET_KEY_BASE"]
+#
+# Thanks to: http://daniel.fone.net.nz/blog/2013/05/20/a-better-way-to-manage-the-rails-secret-token/
+#
+GringottsClient::Application.config.secret_key_base = if Rails.env.development? or Rails.env.test?
+  ('x' * 30) # meets minimum requirement of 30 chars long
+else
+  ENV['SECRET_KEY_BASE']
+end
+
